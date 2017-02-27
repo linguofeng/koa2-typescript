c144bf607867288d646a1f820aa2433fbf4287e4
linguofeng <linguofeng@msn.com>
2017-02-27 18:54:05 +0800

    add tslint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import * as Koa from 'koa';
+import * as json from 'koa-json';
+import * as logger from 'koa-logger';
 
 const app = new Koa();
 
+app.use(logger());
+app.use(json());
+
 app.use(async (ctx) => {
-  ctx.body = 'hello';
+  ctx.body = {
+    data: 'hello',
+  };
 });
 
 app.listen(3000);
